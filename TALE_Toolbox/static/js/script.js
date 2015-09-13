c89a19789eee28e879c26afb932b82870236c404
Joshua Meier <joshim5@gmail.com>
2015-09-13 17:45:19 -0400

    Added validation

--- TALE_Toolbox/static/js/script.js
+++ TALE_Toolbox/static/js/script.js
@@ -6,13 +6,37 @@ $(function(){
     	$("#select-g-monomer .btn:first-child").val($(this).text());
     });
 
+    function show_error(error) {
+    	$("#target-input-form").addClass("has-error");
+    	$("#error-text").removeClass("hidden");
+    	$("#error-text").text(error);
+    }
+
+    function hide_error() {
+    	$("#target-input-form").removeClass("has-error");
+    	$("#error-text").addClass("hidden");
+    }
+
     $("#download-gb-vector").click(function() {
     	var params = {
-    		sequence: $("#target-sequence").val(),
+    		sequence: $("#target-sequence").val().toUpperCase(),
     		g_monomer: $("#select-g-monomer .btn").text().trim(),
     		backbone: $("#TALE-type .active").data("backbone")
     	};
-    	window.location.href = "/generate?" + $.param(params);
+
+    	// validation
+    	if (params.sequence[0] != 'T') {
+    		show_error("Target must begin with a thymine base.");
+    	} else if (params.sequence.length < 2) {
+    		show_error("Target must be at least 2 bases in length.");
+    	} else if (params.sequence.length > 26) {
+    		show_error("Target cannot be greater than 26 bases.");
+    	} else if (!(/^([ATGC]*)$/.test(params.sequence))) {
+    		show_error("Target must consist of nucleotides {A,G,T,C}.")
+    	} else {
+    		hide_error();
+    		window.location.href = "/generate?" + $.param(params);
+    	}
     });
 });
 
